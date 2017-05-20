ea44bb930b2c60a1becd23548bb2813fa8b0b215
Saikamal <easarlasaikamal@gmail.com>
2017-05-20 07:56:03 +0530

    added

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def summarizeCode():
         proxy_handler = urllib2.ProxyHandler({"https" : "https://proxy-domain.name:8080"})
         proxy_opener = urllib2.build_opener(urllib2.HTTPHandler(proxy_handler),
                                         urllib2.HTTPSHandler(proxy_handler))
-        gs_with_proxy = goslate.Goslate(opener=proxy_opener,retry_times=100)
+        gs_with_proxy = goslate.Goslate(opener=proxy_opener,retry_times=100,executor=None, timeout=100, service_urls=(u'http://translate.google.de', ), debug=False)
         input=request.form['text']
         output=gs_with_proxy.translate(input,'en')
         algo=client.algo('nlp/Summarizer/0.1.3')
